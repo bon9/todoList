62b6fbfecb421a741cdbd8976e3910ad1dcd3b9e
bosha <boshaa3@gmail.com>
2018-11-19 20:48:16 +0200

    jsxStart

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+/* // JSX, который в js преобразовует babel
+const el = <h1>Hello World</h1> 
+// без JSX
+const el = React.createElement('h1', null, 'yo'); */
+
+const ToDoList = () => {
+
+	const items = ['Drink Coffee', 'Build Awesome App']
+	return (
+		<ul>
+			<li>{items[0]}</li>
+			<li>{items[1]}</li>
+		</ul>
+	);
+};
+
+const AppHeader = () => <h1> My ToDo List </h1>;
+
+const SearchPanel = () => {
+
+	const searchText = 'Type here to search';
+	const searchStyle = { fontSize: '25px' };
+
+	return <input
+		style={searchStyle}
+		placeholder={searchText} />;
+};
+
+const App = () => {
+
+	return (
+		<div>
+			<AppHeader />
+			<ToDoList />
+			<SearchPanel />
+		</div>
+	);
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();

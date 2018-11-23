a0521f3c726f73bf7b19d4624cde827997e754f4
bosha <boshaa3@gmail.com>
2018-11-23 22:38:52 +0200

    setState() - удаление элемента

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -6,27 +6,47 @@ import ToDoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 import "./app.css"
 
-const App = () => {
+export default class App extends Component {
 
-	const todoData = [
-		{ label: 'Drink Coffee', important: false, id: 1 },
-		{ label: 'Make Awesome App', important: false, id: 2 },
-		{ label: 'Have a lunch', important: true, id: 3 }
-	];
+	constructor() {
+		super();
+		this.deleteItem = this.deleteItem.bind(this);
+	}
 
-	return (
-		<div className="todo-app">
-			<AppHeader toDo={1} done={3} />
-			<div className="top-panel d-flex">
-				<SearchPanel />
-				<ItemStatusFilter />
+	state = {
+		todoData: [
+			{ label: 'Drink Coffee', important: false, id: 1 },
+			{ label: 'Make Awesome App', important: false, id: 2 },
+			{ label: 'Have a lunch', important: true, id: 3 }
+		]
+	};
+
+	deleteItem(id) {
+		this.setState(({ todoData }) => {
+			const idx = todoData.findIndex((el) => el.id === id);
+
+			const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+			// const newArray = todoData.filter(item => item.id !== id); //как вариант можно так, но возможно это дольше из за перебора всех элементов
+
+			return {
+				todoData: newArray
+			};
+		});
+	}
+
+	render() {
+		return (
+			<div className="todo-app">
+				<AppHeader toDo={1} done={3} />
+				<div className="top-panel d-flex">
+					<SearchPanel />
+					<ItemStatusFilter />
+				</div>
+
+				<ToDoList
+					todos={this.state.todoData}
+					onDeleted={this.deleteItem} />
 			</div>
-
-			<ToDoList
-				todos={todoData}
-				onDeleted={(id) => console.log(id)} />
-		</div>
-	);
-};
-
-export default App;
+		);
+	}
+}

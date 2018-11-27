8d6dc3c61cd8ea4a4d6228109f50b1ccb7cc1041
bosha <boshaa3@gmail.com>
2018-11-27 22:31:21 +0200

    Реализация поиска

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,6 +15,7 @@ export default class App extends Component {
 		this.addItem = this.addItem.bind(this);
 		this.onToggleImportant = this.onToggleImportant.bind(this);
 		this.onToggleDone = this.onToggleDone.bind(this);
+		this.onSearchChange = this.onSearchChange.bind(this);
 	}
 
 	maxID = 100;
@@ -24,7 +25,8 @@ export default class App extends Component {
 			this.createTodoItem('Coffee'),
 			this.createTodoItem('Make Awesome App'),
 			this.createTodoItem('Have a lunch')
-		]
+		],
+		term: ''
 	};
 
 	createTodoItem(label) {
@@ -74,7 +76,7 @@ export default class App extends Component {
 			newItem,
 			...arr.slice(idx + 1)
 		];
-	}
+	};
 
 	onToggleDone(id) {
 		this.setState(({ todoData }) => {
@@ -82,7 +84,7 @@ export default class App extends Component {
 				todoData: this.toggleProperty(todoData, id, 'done')
 			};
 		});
-	}
+	};
 
 	onToggleImportant(id) {
 		this.setState(({ todoData }) => {
@@ -90,10 +92,26 @@ export default class App extends Component {
 				todoData: this.toggleProperty(todoData, id, 'important')
 			};
 		});
-	}
+	};
+
+	search(items, term) {
+		if (term.length === 0) {
+			return items;
+		}
+
+		return items.filter((item) => {
+			return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+		});
+	};
+
+	onSearchChange(term) {
+		this.setState({ term });
+	};
 
 	render() {
-		const { todoData } = this.state;
+		const { todoData, term } = this.state;
+
+		const visibleItems = this.search(todoData, term);
 
 		const doneCount = todoData.filter(el => el.done).length;
 		const todoCount = todoData.length - doneCount;
@@ -102,12 +120,13 @@ export default class App extends Component {
 			<div className="todo-app">
 				<AppHeader toDo={todoCount} done={doneCount} />
 				<div className="top-panel d-flex">
-					<SearchPanel />
+					<SearchPanel
+						onSearchChange={this.onSearchChange} />
 					<ItemStatusFilter />
 				</div>
 
 				<ToDoList
-					todos={todoData}
+					todos={visibleItems}
 					onDeleted={this.deleteItem}
 					onToggleImportant={this.onToggleImportant}
 					onToggleDone={this.onToggleDone}
--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -31,11 +31,13 @@ export default class ItemAddForm extends Component {
 		return (
 			<form className="item-add-form d-flex"
 				onSubmit={this.onSubmit}>
+
 				<input type="text"
 					className="form-control"
 					onChange={this.onLabelChange}
 					placeholder="What needs to bedone"
 					value={this.state.label} />
+
 				<button className="btn btn-outline-secondary">
 					Add Item</button>
 			</form>
